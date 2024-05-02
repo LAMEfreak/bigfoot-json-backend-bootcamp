058886b3987db8c9b258b9f46807760fac967cbf
LAMEfreak <leeweirong@outlook.com>
2024-05-02 23:24:17 +0800

    Implemented Filter

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+import express from "express";
+import getSightings from "./utils.js";
+import { config } from "dotenv";
+import cors from "cors";
 
+config();
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
-  res.json(sightings);
+  
+  // Define year and month query parameters
+  const { year, month } = req.query;
+
+  // Initialize filteredSightings to all sightings
+  let filteredSightings = sightings;
+
+  // If year query parameter is present, filter sightings by year
+  if (year) {
+    filteredSightings = filteredSightings.filter(
+      (sighting) => sighting["YEAR"] === year
+    );
+  }
+
+  // If month query parameter is present, filter sightings by month
+  if (month) {
+    filteredSightings = filteredSightings.filter(
+      (sighting) => sighting["MONTH"] === month
+    );
+  }
+
+  // Return filtered sightings
+  res.json(filteredSightings);
+});
+
+app.get("/sightings/:sightingsIndex", async (req, res) => {
+  const sightings = await getSightings();
+  const sightingsIndex = req.params.sightingsIndex;
+  res.json(sightings[sightingsIndex]);
 });
 
 app.listen(PORT, () => {
